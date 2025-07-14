6c0d04a5650d334b4b7e95ccee249cbf8807ebef
Miguel Barcelo <miguel.barcelo@gmail.com>
2025-07-14 15:33:18 -0700

    chore: init project

---
+++ src/main/java/com/miguel_barcelo/async_processing_large_files/AsyncProcessingLargeFilesApplication.java
@@ -0,0 +1,13 @@
+package com.miguel_barcelo.async_processing_large_files;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsyncProcessingLargeFilesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AsyncProcessingLargeFilesApplication.class, args);
+	}
+
+}
